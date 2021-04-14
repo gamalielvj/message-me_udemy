53c083d68aaab829dcdad14b5d4b21109173c9fb
Gamaliel Vazquez <gamalielvj@yahoo.com.mx>
2021-04-13 19:47:26 -0700

    Action Cable is workinggit add -A! and that's all that matters

--- README.md
+++ README.md
@@ -15,11 +15,12 @@ Things you may want to cover:
       2.6.3
 
 * System dependencies
-      gem 'turbolinks', '~> 5'
-      gem 'jquery-rails'
-      gem 'bcrypt', '~> 3.1.7'
-      gem 'semantic-ui-sass'
-      gem 'hirb'
+      gems:
+       turbolinks,
+       jquery-rails,
+       bcrypt,
+       semantic-ui-sass
+       hirb <-- Not necessary but usefully tool for Rails console.
 
 * Configuration
 
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,14 +3,10 @@ class MessagesController < ApplicationController
 
   def create
     message = current_user.messages.build(message_params)
-    if message.save
-      redirect_to root_path
-    end
-
-    # if message.save
-    #   ActionCable.server.broadcast "chatroom_channel",
-    #                                 mod_message: message_render(message)
-    # end
+     if message.save
+       ActionCable.server.broadcast "chatroom_channel",
+                                     mod_message: message_render(message)
+     end
   end
 
   private
